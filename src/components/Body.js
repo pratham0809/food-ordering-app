73c48484ed5706c655b7c3b91383b0d685a4cab9
Pratham Thakkar <thakkarpratham20002@gmail.com>
2023-06-29 16:11:41 +0530

    Features added

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,21 +1,44 @@
-import RestaurantCard from "./RestaurantCard"
-import data from "../utils/config"
+import RestaurantCard from "./RestaurantCard";
+import data from "../utils/config";
+import { useState } from "react";
 
-const Body=()=>{
-    return(
-        <div id="body">
-            <div className="search-bar">
-                <h2>Search Bar</h2>
-            </div>
-            <div className="res-container">
-                {
-                    data.map((res)=>{
-                        return(<RestaurantCard info={res} key={res.data.data.id}/>)
-                    })
-                }
-            </div>
-        </div>
-    )
-}
+const Body = () => {
+  const [resList, setResList] = useState(data);
+  return (
+    <div id="body">
+      <div className="search-bar">
+        <input className="res-name" />
+        <button
+          onClick={() => {
+            const input = document.querySelector(".res-name");
+            let resFiltered = resList.filter((res) => {
+              return res.data.data.name == input.value;
+            });
+            setResList(resFiltered);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="filter-btn">
+        <button
+          onClick={() => {
+            let updateData = resList.filter((res) => {
+              return res.data.data.avgRating > 4;
+            });
+            setResList(updateData);
+          }}
+        >
+          filter
+        </button>
+      </div>
+      <div className="res-container">
+        {resList.map((res) => {
+          return <RestaurantCard info={res} key={res.data.data.id} />;
+        })}
+      </div>
+    </div>
+  );
+};
 
-export default Body
+export default Body;
